697368fb34bf56bcc92f6771547c26bd33790d7d
dev.Joel <joelsojo2003@gmail.com>
2025-10-09 01:39:01 -0400

    TOAST!

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,11 +5,13 @@ import './index.css'
 import App from './App'
 import "./i18n";
 import { ThemeProvider } from './components/ThemeProvider'
+import { Toaster } from 'sonner';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <Toaster position="top-center" />
         <App />
       </ThemeProvider>
     </BrowserRouter>
